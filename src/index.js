316340eebcb5a32846625379bcc1baf05e50e0ad
Aadeesh Jain <aadeeshjain51@gmail.com>
2026-06-09 20:28:13 +0530

    routing

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv"
 import mongoose from "mongoose";
 import {DB_NAME} from "./constants.js"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 // import express from "express"
 // const app = express()
 
